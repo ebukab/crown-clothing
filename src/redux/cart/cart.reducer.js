00a0d0390232b5a70f93a0fb3707cec1673d5e30
Chukwuebuka Beluolisa <chukwuebukabeluolisa@Chukwuebukas-MacBook-Pro.local>
2020-03-21 11:10:59 -0400

    Added add items to cart in redux

---
+++ src/redux/cart/cart.reducer.js
@@ -0,0 +1,25 @@
+import cartActionTypes from "./cart.types"
+
+const INITITAL_STATE = {
+    hidden: true,
+    cartItems: []
+}
+
+const cartReducer = (state= INITITAL_STATE, action) => { 
+    switch (action.type) { 
+        case cartActionTypes.TOGGLE_CART_HIDDEN:
+            return {
+                ...state,
+                hidden: !state.hidden
+            }
+        case cartActionTypes.ADD_ITEM:
+            return {
+                ...state,
+                cartItems: [...state.cartItems, action.payload]
+            }
+        default:
+            return state;
+    }
+}
+
+export default cartReducer
